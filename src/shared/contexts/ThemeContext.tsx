8e7d96a3570d48f5f70e33b9382166e1da550796
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-11-09 16:24:51 -0300

    :sparkles: feat:  Creates the context theme for the project

---
+++ src/shared/contexts/ThemeContext.tsx
@@ -0,0 +1,52 @@
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+} from "react";
+import { Box, ThemeProvider } from "@mui/material";
+import { LightTheme, DarkTheme } from "../themes";
+
+interface IThemeContextData {
+  themeName: "light" | "dark";
+  toggleTheme: () => void;
+}
+
+type ChildrenProps = {
+  children: React.ReactNode;
+};
+
+const ThemeContext = createContext({} as IThemeContextData);
+
+export const useThemeContext = () => {
+  return useContext(ThemeContext);
+};
+
+export function AppThemeProvider({ children }: ChildrenProps) {
+  const [themeName, setThemeName] = useState<"light" | "dark">("light");
+
+  const toggleTheme = useCallback(() => {
+    setThemeName((oldTheme) => (oldTheme === "light" ? "dark" : "light"));
+  }, []);
+
+  const theme = useMemo(() => {
+    if (themeName === "light") return LightTheme;
+
+    return DarkTheme;
+  }, [themeName]);
+
+  return (
+    <ThemeContext.Provider value={{ themeName, toggleTheme }}>
+      <ThemeProvider theme={DarkTheme}>
+        <Box
+          width={"100vw"}
+          height={"100vh"}
+          bgcolor={theme.palette.background.default}
+        >
+          {children}
+        </Box>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+}
