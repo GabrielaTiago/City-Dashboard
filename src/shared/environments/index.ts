06c1d00b7b17f4ff33c6473cbec400c22b16eda3
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-11-24 12:03:25 -0300

    :sparkles: feat: Creates environment variables

--- src/shared/environments/index.ts
+++ src/shared/environments/index.ts
@@ -1,1 +1,14 @@
-export {};
+export const enviroment =  {
+  /*
+    - Defines the number of rows to be loaded by default in the lists
+  */
+    LINE_LIMIT: 10,
+  /*
+    - Placeholder displayed in the inputs
+  */
+   SEARCH_INPUT: '',
+   /*
+     - Text displayed when no item is found in a listing
+   */
+   EMPTY_LIST: 'Nenhum registro encontrado'
+};
