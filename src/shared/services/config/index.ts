81644b9babcad2e67041339bb7c893f213e4a6ba
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-12-08 23:07:32 -0300

    :sparkles: feat:  Appends the header in the request to access the token

--- src/shared/services/config/index.ts
+++ src/shared/services/config/index.ts
@@ -3,6 +3,9 @@ import { errorInterceptor, responseInterceptor } from './interceptors';
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${JSON.stringify(localStorage.getItem('APP_ACCESS_TOKEN') || '')}`
+  }
 })
 
 instance.interceptors.response.use(
